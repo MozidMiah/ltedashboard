a58074298c54ef9f65198c8612aa6dc10de77cd6
MozidMiah <mazidkhan278@gmail.com>
2026-03-08 02:56:20 +0600

    yajra

--- resources/views/admin/category/index.blade.php
+++ resources/views/admin/category/index.blade.php
@@ -1,46 +1,77 @@
 @extends('admin.dashboard')
 
 @section('content')
-<div class="container">
-    <h2>Category List</h2>
-    <a href="{{ route('category.create') }}" class="btn btn-success btn-sm mb-2">Add New Category</a>
+    <div class="container">
+        <div class="col-sm-12">
+            <ol class="breadcrumb float-sm-right">
+                <li class="breadcrumb-item">
+                    <a href="{{ route('dashboard') }}">Home</a>
+                </li>
+                <li class="breadcrumb-item">
+                    <a href="{{ route('category.index') }}">Category</a>
+                </li>
+            </ol>
+        </div>
+        <h2>Category List</h2>
+        <a href="{{ route('category.create') }}" class="btn btn-success btn-sm mb-2">Add New Category</a>
 
-    @if(session('success'))
-        <div class="alert alert-success">{{ session('success') }}</div>
-    @endif
+        @if (session('success'))
+            <div class="alert alert-success">{{ session('success') }}</div>
+        @endif
 
-    <table class="table table-bordered" id="categoryTable">
-        <thead>
-            <tr>
-                <th>No</th>
-                <th>Image</th>
-                <th>Name</th>
-                <th>Description</th>
-                <th>status</th>
-                <th>Action</th>
-            </tr>
-        </thead>
-    </table>
-</div>
+        <table class="table table-bordered" id="categoryTable">
+            <thead>
+                <tr>
+                    <th>No</th>
+                    <th>Image</th>
+                    <th>Name</th>
+                    <th>Description</th>
+                    <th>status</th>
+                    <th>Action</th>
+                </tr>
+            </thead>
+        </table>
+    </div>
 @endsection
 
 @push('scripts')
-<script>
-    $(document).ready(function() {
-        $('#categoryTable').DataTable({
-            processing: true,
-            serverSide: true,
-            pagingType: "numbers",
-            ajax: "{{ route('category.data') }}",
-            columns: [
-                { data: 'DT_RowIndex', name: 'DT_RowIndex', orderable: false, searchable: false },
-                { data: 'image', name: 'image' },
-                { data: 'name', name: 'name' },
-                { data: 'description', name: 'description' },
-                { data: 'status', name: 'status' },
-                { data: 'action', name: 'action', orderable: false, searchable: false },
-            ]
+    <script>
+        $(document).ready(function() {
+            $('#categoryTable').DataTable({
+                processing: true,
+                serverSide: true,
+                pagingType: "numbers",
+                ajax: "{{ route('category.data') }}",
+                columns: [{
+                        data: 'DT_RowIndex',
+                        name: 'DT_RowIndex',
+                        orderable: false,
+                        searchable: false
+                    },
+                    {
+                        data: 'image',
+                        name: 'image'
+                    },
+                    {
+                        data: 'name',
+                        name: 'name'
+                    },
+                    {
+                        data: 'description',
+                        name: 'description'
+                    },
+                    {
+                        data: 'status',
+                        name: 'status'
+                    },
+                    {
+                        data: 'action',
+                        name: 'action',
+                        orderable: false,
+                        searchable: false
+                    },
+                ]
+            });
         });
-    });
-</script>
+    </script>
 @endpush
