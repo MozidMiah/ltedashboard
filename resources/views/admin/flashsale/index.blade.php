ca5e8d6abba8d43bb50270c8f26b8ef76dc05535
MozidMiah <mazidkhan278@gmail.com>
2026-04-02 02:08:06 +0600

    Flash Sale

---
+++ resources/views/admin/flashsale/index.blade.php
@@ -0,0 +1,87 @@
+@extends('admin.dashboard')
+
+@section('content')
+    <div class="container">
+        <div class="col-sm-12">
+            <ol class="breadcrumb float-sm-right">
+                <li class="breadcrumb-item">
+                    <a href="{{ route('dashboard') }}">Home</a>
+                </li>
+                <li class="breadcrumb-item">
+                    <a href="{{ route('flash-sale.index') }}">Flashsale</a>
+                </li>
+            </ol>
+        </div>
+        <h2>Flash Sale List</h2>
+        <a href="{{ route('flash-sale.create') }}" class="btn btn-success btn-sm mb-2">Add New</a>
+
+        @if (session('success'))
+            <div class="alert alert-success">{{ session('success') }}</div>
+        @endif
+
+        <table class="table table-bordered" id="flash-saleTable">
+            <thead>
+                <tr>
+                    <th>No</th>
+                    <th>Thumbnail</th>
+                    <th>Name</th>
+                    <th>Start Date</th>
+                    <th>End Date</th>
+                    <th>status</th>
+                    <th>Description</th>
+                    <th>Action</th>
+                </tr>
+            </thead>
+        </table>
+    </div>
+@endsection
+
+@push('scripts')
+    <script>
+        $(document).ready(function() {
+            $('#flash-saleTable').DataTable({
+                processing: true,
+                serverSide: true,
+                pagingType: "numbers",
+                ajax: "{{ route('flash-sale.data') }}",
+                columns: [{
+                        data: 'DT_RowIndex',
+                        name: 'DT_RowIndex',
+                        orderable: false,
+                        searchable: false
+                    },
+                    {
+                        data: 'thumbnail',
+                        name: 'thumbnail'
+                    },
+                    {
+                        data: 'name',
+                        name: 'name'
+                    },
+                    {
+                        data: 'start_date',
+                        name: 'start_date'
+                    },
+                    {
+                        data: 'end_date',
+                        name: 'end_date'
+                    },
+                    {
+                        data: 'status',
+                        name: 'status'
+                    },
+                    {
+                        data: 'description',
+                        name: 'description'
+                    },
+                    {
+                        data: 'action',
+                        name: 'action',
+                        orderable: false,
+                        searchable: false
+                    },
+                ]
+            });
+        });
+    </script>
+@endpush
