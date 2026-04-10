b01081e6d1a389b6f6ff763bf05caadc00151c59
MozidMiah <mazidkhan278@gmail.com>
2026-04-11 02:36:44 +0600

    Support

--- resources/views/admin/support/index.blade.php
+++ resources/views/admin/support/index.blade.php
@@ -2,51 +2,85 @@
 
 @section('content')
     <div class="container mt-3">
-        <div class="col-sm-12">
-            <ol class="breadcrumb float-sm-right">
-                <li class="breadcrumb-item"><a href="{{ route('dashboard') }}">Home</a></li>
-                <li class="breadcrumb-item active">Support</li>
-            </ol>
-        </div>
+    <div class="col-sm-12">
+        <ol class="breadcrumb float-sm-right">
+            <li class="breadcrumb-item"><a href="{{ route('dashboard') }}">Home</a></li>
+            <li class="breadcrumb-item active">Support</li>
+        </ol>
+    </div>
 
-        <div class="container mt-4">
+    <h2>Support Message</h2>
+    {{-- <a href="{{ route('size.create') }}" class="btn btn-success btn-sm mb-2">Add New Size</a> --}}
 
-            <button class="btn btn-primary mb-3" data-toggle="modal" data-target="#addModal">
-                Add Message
-            </button>
+    {{-- @if (session('success'))
+        <div class="alert alert-success">{{ session('success') }}</div>
+    @endif --}}
 
-            <table class="table table-bordered" id="supportTable">
-                <thead>
-                    <tr>
-                        <th>#</th>
-                        <th>Subject</th>
-                        <th>Message</th>
-                        <th>Action</th>
-                    </tr>
-                </thead>
-            </table>
+    <table class="table table-bordered" id="sizeTable">
+        <thead>
+            <tr>
+                <th>No</th>
+                <th>Subject</th>
+                <th>Message</th>
+                <th>Action</th>
+            </tr>
+        </thead>
+    </table>
+</div>
 
-        </div>
+    <!-- Modal -->
+    <div class="modal fade" id="addModal">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <form id="addForm">
+                    <div class="modal-header">
+                        <h5>Add Support Message</h5>
+                    </div>
 
-        <!-- Modal -->
-        <div class="modal fade" id="addModal">
-            <div class="modal-dialog">
-                <div class="modal-content">
-                    <form id="addForm">
-                        <div class="modal-header">
-                            <h5>Add Support Message</h5>
-                        </div>
+                    <div class="modal-body">
+                        <input type="text" name="subject" class="form-control mb-2" placeholder="Subject">
+                        <textarea name="message" class="form-control" placeholder="Message"></textarea>
+                    </div>
 
-                        <div class="modal-body">
-                            <input type="text" name="subject" class="form-control mb-2" placeholder="Subject">
-                            <textarea name="message" class="form-control" placeholder="Message"></textarea>
-                        </div>
-
-                        <div class="modal-footer">
-                            <button class="btn btn-success">Send</button>
-                        </div>
-                    </form>
-                </div>
+                    <div class="modal-footer">
+                        <button class="btn btn-success">Send</button>
+                    </div>
+                </form>
             </div>
         </div>
-    @endpush
+    </div>
+@endsection
+
+@push('scripts')
+    <script>
+        $(document).ready(function() {
+            $('#supportTable').DataTable({
+                processing: true,
+                serverSide: true,
+                // pagingType: "numbers",
+                ajax: "{{ route('support.data') }}",
+                columns: [{
+                        data: 'DT_RowIndex',
+                        name: 'DT_RowIndex',
+                        orderable: false,
+                        searchable: false
+                    },
+                    {
+                        data: 'Subject',
+                        name: 'Subject'
+                    },
+                    {
+                        data: 'Message',
+                        name: 'Message'
+                    },
+                    {
+                        data: 'action',
+                        name: 'action',
+                        orderable: false,
+                        searchable: false
+                    },
+                ]
+            });
+        });
+    </script>
+@endpush
